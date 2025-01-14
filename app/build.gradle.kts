b315c43d46b93d680ccba3574349cf324292d740
smh10 <smh10.mh@gmail.com>
2025-01-14 13:57:30 -0500

    DataStore dependency is added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
 
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
